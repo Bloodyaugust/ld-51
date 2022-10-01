fc702ad0c8515ee741f8e7cdad3fef37d87bc61b
Greyson Richey <greysonrichey@gmail.com>
2022-09-30 20:30:00 -0600

    Modify template CCI and build stuff
    - Stupid mac import changes

--- tools/build-and-deploy.sh
+++ tools/build-and-deploy.sh
@@ -18,24 +18,24 @@ mkdir build/win/
 
 echo "EXPORTING FOR LINUX"
 echo "-----------------------------"
-godot --export "Linux/X11" build/linux/godot-template.x86_64 -v
+godot --export "Linux/X11" build/linux/ld-51.x86_64 -v
 # echo "EXPORTING FOR OSX"
 # echo "-----------------------------"
-# godot --export "Mac OSX" build/osx/godot-template.dmg -v
+# godot --export "Mac OSX" build/osx/ld-51.dmg -v
 echo "EXPORTING FOR WINDOZE"
 echo "-----------------------------"
-godot --export-debug "Windows Desktop" build/win/godot-template.exe -v
+godot --export-debug "Windows Desktop" build/win/ld-51.exe -v
 echo "-----------------------------"
 
 # echo "CHANGING FILETYPE AND CHMOD EXECUTABLE FOR OSX"
 # echo "-----------------------------"
 # cd build/osx/
-# mv godot-template.dmg godot-template-osx-alpha.zip
-# unzip godot-template-osx-alpha.zip
-# rm godot-template-osx-alpha.zip
-# chmod +x godot-template.app/Contents/MacOS/godot-template
-# zip -r godot-template-osx-alpha.zip godot-template.app
-# rm -rf godot-template.app
+# mv ld-51.dmg ld-51-osx-alpha.zip
+# unzip ld-51-osx-alpha.zip
+# rm ld-51-osx-alpha.zip
+# chmod +x ld-51.app/Contents/MacOS/ld-51
+# zip -r ld-51-osx-alpha.zip ld-51.app
+# rm -rf ld-51.app
 # cd ../../
 
 ls -al
@@ -47,15 +47,15 @@ ls -al build/win/
 echo "ZIPPING FOR WINDOZE"
 echo "-----------------------------"
 cd build/win/
-zip -r godot-template-win-alpha.zip godot-template.exe godot-template.pck
-rm -r godot-template.exe godot-template.pck
+zip -r ld-51-win-alpha.zip ld-51.exe ld-51.pck
+rm -r ld-51.exe ld-51.pck
 cd ../../
 
 echo "ZIPPING FOR LINUX"
 echo "-----------------------------"
 cd build/linux/
-zip -r godot-template-linux-alpha.zip godot-template.x86_64 godot-template.pck
-rm -r godot-template.x86_64 godot-template.pck
+zip -r ld-51-linux-alpha.zip ld-51.x86_64 ld-51.pck
+rm -r ld-51.x86_64 ld-51.pck
 cd ../../
 
 echo "Logging in to Butler"
@@ -64,6 +64,6 @@ butler login
 
 echo "Pushing builds with Butler"
 echo "-----------------------------"
-butler push build/linux/ synsugarstudio/godot-template:linux-alpha
-# butler push build/osx/ synsugarstudio/godot-template:osx-alpha
-butler push build/win/ synsugarstudio/godot-template:win-alpha
+butler push build/linux/ synsugarstudio/ld-51:linux-alpha
+# butler push build/osx/ synsugarstudio/ld-51:osx-alpha
+butler push build/win/ synsugarstudio/ld-51:win-alpha
