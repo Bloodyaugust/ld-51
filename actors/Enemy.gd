a57edd50903604dad151936b3a1d5607052a2a80
Greyson Richey <greysonrichey@gmail.com>
2022-09-30 23:40:40 -0600

    Change player and enemies to KinematicBody2D
    - Move with move_and_slide
    - Add a bunch of enemies to main scene

--- actors/Enemy.gd
+++ actors/Enemy.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 export var data_default: Resource
 
@@ -9,11 +9,11 @@ onready var _player: Node2D = get_tree().get_nodes_in_group("player")[0]
 
 var _time_to_attack: float
 
-func _process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
   var _direction: Vector2 = global_position.direction_to(_player.global_position)
-  
-  global_position += _direction * data.speed * delta
-  
+  var _movement: Vector2 = _direction * data.speed
+
+  move_and_slide(_movement)
   
 func _ready() -> void:
   if !data:
--- actors/Player.gd
+++ actors/Player.gd
@@ -1,10 +1,10 @@
-extends Sprite
+extends KinematicBody2D
 
 const MOVE_SPEED_BASE: float = 25.0
 
-func _process(delta):
+func _process(_delta):
   var _movement: Vector2 = Vector2.ZERO
   
-  _movement += Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized() * MOVE_SPEED_BASE * delta
+  _movement += Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized() * MOVE_SPEED_BASE
   
-  global_position += _movement
+  move_and_slide(_movement)
