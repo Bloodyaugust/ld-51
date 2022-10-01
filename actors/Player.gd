75d966bc515f5a938497765a8560ac3f0f4591fa
Greyson Richey <greysonrichey@gmail.com>
2022-09-30 22:39:01 -0600

    Disconnect Player from UnitData
    - Make movespeed the same as level 0 enemy

--- actors/Player.gd
+++ actors/Player.gd
@@ -1,10 +1,10 @@
 extends Sprite
 
-export var data: Resource
+const MOVE_SPEED_BASE: float = 25.0
 
 func _process(delta):
   var _movement: Vector2 = Vector2.ZERO
   
-  _movement += Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized() * data.speed * delta
+  _movement += Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized() * MOVE_SPEED_BASE * delta
   
   global_position += _movement
