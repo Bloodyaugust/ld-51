dd1a5dd1e395e7702beffd9a0d43eaf51fda9f4c
PhatKiwi <ethanandy90@gmail.com>
2022-10-01 18:25:25 -0600

    Create oxygen interval -player takes damage every 10 seconds:

--- actors/Player.gd
+++ actors/Player.gd
@@ -3,9 +3,11 @@ extends KinematicBody2D
 const MOVE_SPEED_BASE: float = 25.0
 const OXYGEN_CAPACITY_BASE: int = 6
 const WEAPON_SCRIPT := preload("res://scripts/weapon.gd")
+const OXYGEN_USE_INTERVAL: float = 10.0
 
 var last_move_direction: Vector2 = Vector2.RIGHT
 var oxygen_capacity: int = OXYGEN_CAPACITY_BASE
+var oxygen_interval: float = OXYGEN_USE_INTERVAL
 
 var _oxygen: int
 
@@ -26,10 +28,16 @@ func _on_store_state_changed(state_key: String, substate) -> void:
       new_weapon.player = self
       add_child(new_weapon)
   
-func _process(_delta):
+func _process(delta):
   var _movement: Vector2 = Vector2.ZERO
   
   _movement += Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized() * MOVE_SPEED_BASE
+  
+  oxygen_interval -= delta
+  
+  if oxygen_interval <= 0:
+    hit(1)
+    oxygen_interval = OXYGEN_USE_INTERVAL
   
   if _movement.normalized().length() > 0.1:
     last_move_direction = _movement.normalized()
