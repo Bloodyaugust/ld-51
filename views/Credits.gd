4f6d0d61ac98a52941533381c63b7dc0720f93b6
Bloodyaugust <greysonrichey@gmail.com>
2022-10-03 13:09:41 -0600

    Add Credits view
    - Implement quit game

--- views/Credits.gd
+++ views/Credits.gd
@@ -1,30 +1,23 @@
 extends Control
 
 onready var _animation_player: AnimationPlayer = find_node("AnimationPlayer")
-onready var _music_volume_slider: HSlider = $"%MusicVolume"
 onready var _main_menu_button: Button = find_node("MainMenu")
 
 func _on_main_menu_pressed() -> void:
   Store.set_state("client_view", ClientConstants.CLIENT_VIEW_MAIN_MENU)
 
-func _on_music_volume_slider_changed(value: float) -> void:
-  Store.set_state("music_volume", value)
-
 func _on_state_changed(state_key: String, substate):
   match state_key:
     "client_view":
       match substate:
-        ClientConstants.CLIENT_VIEW_OPTIONS:
+        ClientConstants.CLIENT_VIEW_CREDITS:
           visible = true
           _animation_player.play("show")
         ClientConstants.CLIENT_VIEW_SPLASH:
           pass
         _:
           _animation_player.play("hide")
-    "music_volume":
-      _music_volume_slider.value = substate
 
 func _ready():
   Store.connect("state_changed", self, "_on_state_changed")
   _main_menu_button.connect("pressed", self, "_on_main_menu_pressed")
-  _music_volume_slider.connect("value_changed", self, "_on_music_volume_slider_changed")
--- views/MainMenu.gd
+++ views/MainMenu.gd
@@ -4,6 +4,8 @@ onready var _animation_player:AnimationPlayer = find_node("AnimationPlayer")
 onready var _play_button: Button = find_node("Play")
 onready var _options_button: Button = find_node("Options")
 onready var _howtoplay_button: Button = find_node("HowToPlay")
+onready var _credits_button: Button = find_node("Credits")
+onready var _quit_button: Button = find_node("QuitGame")
 
 func _on_howtoplay_button_pressed() -> void:
   Store.set_state("client_view", ClientConstants.CLIENT_VIEW_HOWTOPLAY)
@@ -13,6 +15,12 @@ func _on_play_button_pressed() -> void:
 
 func _on_options_button_pressed() -> void:
   Store.set_state("client_view", ClientConstants.CLIENT_VIEW_OPTIONS)
+
+func _on_credits_button_pressed() -> void:
+  Store.set_state("client_view", ClientConstants.CLIENT_VIEW_CREDITS)
+
+func _on_quit_button_pressed() -> void:
+  get_tree().quit()
 
 func _on_state_changed(state_key: String, substate):
   match state_key:
@@ -30,5 +38,7 @@ func _ready():
   _play_button.connect("pressed", self, "_on_play_button_pressed")
   _options_button.connect("pressed", self, "_on_options_button_pressed")
   _howtoplay_button.connect("pressed", self, "_on_howtoplay_button_pressed")
+  _credits_button.connect("pressed", self, "_on_credits_button_pressed")
+  _quit_button.connect("pressed", self, "_on_quit_button_pressed")
 
   Store.connect("state_changed", self, "_on_state_changed")
